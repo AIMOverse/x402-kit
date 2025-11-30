67640494d533d5b18355b0b32007d17df9b10435
takasaki404 <fracher21@gmail.com>
2025-12-01 04:45:30 +0800

    chore: solana middleware example

--- crates/x402-kit/examples/axum_middleware.rs
+++ crates/x402-kit/examples/axum_middleware.rs
@@ -1,18 +1,20 @@
 use alloy_primitives::address;
 use axum::{
     Extension, Json, Router,
-    extract::Request,
-    middleware::{Next, from_fn},
+    extract::{Request, State},
+    middleware::{Next, from_fn_with_state},
     response::{IntoResponse, Response},
     routing::post,
 };
 use http::StatusCode;
+use solana_pubkey::pubkey;
+use url::Url;
 use url_macro::url;
 use x402_kit::{
     config::Resource,
     facilitator_client::RemoteFacilitatorClient,
-    networks::evm::assets::UsdcBase,
-    schemes::exact_evm::ExactEvm,
+    networks::{evm::assets::UsdcBase, svm::assets::UsdcSolanaDevnet},
+    schemes::{exact_evm::ExactEvm, exact_svm::ExactSvm},
     seller::axum::{PaymentHandler, PaymentProcessingState},
 };
 
@@ -20,11 +22,26 @@ use x402_kit::{
 async fn main() {
     tracing_subscriber::fmt::init();
 
+    let facilitator_url = std::env::var("FACILITATOR_URL")
+        .expect("FACILITATOR_URL not set")
+        .parse()
+        .expect("Invalid FACILITATOR_URL");
+
+    tracing::info!("Using facilitator at {}", facilitator_url);
+
+    let state = AppState { facilitator_url };
+
     // build our application with a route
-    let app = Router::new().route(
-        "/premium",
-        post(premium_content).layer(from_fn(payment_middleware)),
-    );
+    let app = Router::new()
+        .route(
+            "/premium",
+            post(premium_content).layer(from_fn_with_state(state.clone(), payment_middleware)),
+        )
+        .route(
+            "/premium/solana",
+            post(premium_content)
+                .layer(from_fn_with_state(state.clone(), payment_middleware_solana)),
+        );
 
     // run our app with hyper, listening globally on port 3000
     tracing::info!("Listening on http://0.0.0.0:3000");
@@ -32,35 +49,73 @@ async fn main() {
     axum::serve(listener, app).await.unwrap();
 }
 
-async fn payment_middleware(req: Request, next: Next) -> Response {
-    PaymentHandler::builder(RemoteFacilitatorClient::from_url(
-        std::env::var("FACILITATOR_URL")
-            .expect("FACILITATOR_URL not set")
-            .parse()
-            .expect("Invalid FACILITATOR_URL"),
-    ))
-    .add_payment(
-        ExactEvm::builder()
-            .asset(UsdcBase)
-            .amount(1000)
-            .pay_to(address!("0x17d2e11d0405fa8d0ad2dca6409c499c0132c017"))
-            .resource(
-                Resource::builder()
-                    .url(url!("http://localhost:3000/premium"))
-                    .description("")
-                    .mime_type("")
-                    .build(),
-            )
-            .build(),
-    )
-    .build()
-    .handle_payment()
-    .req(req)
-    .next(next)
-    .call()
-    .await
-    .map(|r| r.into_response())
-    .unwrap_or_else(|err| err.into_response())
+#[derive(Clone)]
+struct AppState {
+    facilitator_url: Url,
+}
+
+async fn payment_middleware(State(state): State<AppState>, req: Request, next: Next) -> Response {
+    PaymentHandler::builder(RemoteFacilitatorClient::from_url(state.facilitator_url))
+        .add_payment(
+            ExactEvm::builder()
+                .asset(UsdcBase)
+                .amount(1000)
+                .pay_to(address!("0x17d2e11d0405fa8d0ad2dca6409c499c0132c017"))
+                .resource(
+                    Resource::builder()
+                        .url(
+                            url!("http://localhost:3000")
+                                .join(req.uri().path())
+                                .unwrap(),
+                        )
+                        .description("")
+                        .mime_type("")
+                        .build(),
+                )
+                .build(),
+        )
+        .build()
+        .handle_payment()
+        .req(req)
+        .next(next)
+        .call()
+        .await
+        .map(|r| r.into_response())
+        .unwrap_or_else(|err| err.into_response())
+}
+
+async fn payment_middleware_solana(
+    State(state): State<AppState>,
+    req: Request,
+    next: Next,
+) -> Response {
+    PaymentHandler::builder(RemoteFacilitatorClient::from_url(state.facilitator_url))
+        .add_payment(
+            ExactSvm::builder()
+                .asset(UsdcSolanaDevnet)
+                .amount(1000)
+                .pay_to(pubkey!("Ge3jkza5KRfXvaq3GELNLh6V1pjjdEKNpEdGXJgjjKUR"))
+                .resource(
+                    Resource::builder()
+                        .url(
+                            url!("http://localhost:3000")
+                                .join(req.uri().path())
+                                .unwrap(),
+                        )
+                        .description("")
+                        .mime_type("")
+                        .build(),
+                )
+                .build(),
+        )
+        .build()
+        .handle_payment()
+        .req(req)
+        .next(next)
+        .call()
+        .await
+        .map(|r| r.into_response())
+        .unwrap_or_else(|err| err.into_response())
 }
 
 async fn premium_content(
